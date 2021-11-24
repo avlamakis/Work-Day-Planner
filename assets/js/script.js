1010657102e68b6470b83aa8ac3ccb97121231df
alexvlamakis <alexvlamakis@gmail.com>
2021-11-23 22:20:34 -0700

    local storage added to set Item and append child listed for refresh on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,4 +36,30 @@ $(document).ready(function () {
         }
 
     })
-});
+    // on click function for local storage
+    $("button").on("click", function () {
+
+    // sets items to the local storage for each time block value
+        localStorage.setItem("9AM", ($time9AM.val()))
+        localStorage.setItem("10AM", ($time10AM.val()))
+        localStorage.setItem("11AM", ($time11AM.val()))
+        localStorage.setItem("12PM", ($time12PM.val()))
+        localStorage.setItem("1PM", ($time1PM.val()))
+        localStorage.setItem("2PM", ($time2PM.val()))
+        localStorage.setItem("3PM", ($time3PM.val()))
+        localStorage.setItem("4PM", ($time4PM.val()))
+        localStorage.setItem("5PM", ($time5PM.val()))
+
+    })
+        //getting the content stored and sending to the screen. When page is refreshed content will stay
+        $("#time9AM").append(localStorage.getItem("9AM"));
+        $("#time10AM").append(localStorage.getItem("10AM"));
+        $("#time11AM").append(localStorage.getItem("11AM"));
+        $("#time12PM").append(localStorage.getItem("12PM"));
+        $("#time1PM").append(localStorage.getItem("1PM"));
+        $("#time2PM").append(localStorage.getItem("2PM"));
+        $("#time3PM").append(localStorage.getItem("3PM"));
+        $("#time4PM").append(localStorage.getItem("4PM"));
+        $("#time5PM").append(localStorage.getItem("5PM"));
+    
+    });
